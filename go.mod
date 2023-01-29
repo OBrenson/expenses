6e5a807fa83f02e8359935f0ae8c76769c393182
boi <oleg-brenyov@yandex.ru>
2023-01-29 09:39:45 +0400

    changes in dao

--- go.mod
+++ go.mod
@@ -39,7 +39,6 @@ require (
 	golang.org/x/crypto v0.4.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
-	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/gorm v1.24.3 // indirect
--- internal/dbaccess/dbaccess.go
+++ internal/dbaccess/dbaccess.go
@@ -8,21 +8,27 @@ import (
 	"gorm.io/gorm/schema"
 )
 
-type Api[T domain.Data | domain.User] interface {
-
-	Insert(val *T) (*T,error)
-	GetByField (field string, val interface{}) (*T,error)
-	DeleteByField (field string, val interface{}) (*T,error)
+type DaoApi[T domain.Data | domain.User] interface {
+	Insert(val *T) (*T, error)
+	GetByField(field string, val interface{}) (*T, error)
+	DeleteByField(field string, val interface{}) (*T, error)
 	UpdateField(id int, field string, val interface{}) (*T, error)
+}
 
+type DbApi interface {
+	Query(func (args... interface{}) *gorm.DB) *gorm.DB
+}
+
+type DbService struct {
+	gorm.DB
 }
 
 type DaoService[T domain.Data | domain.User] struct {
-	db *gorm.DB
+	db *DbService
 }
 
-func CreateDb(dsn string, schemaName string) (db *gorm.DB) {
-	db, err := gorm.Open(
+func CreateDb(dsn string, schemaName string) (db DbApi) {
+	gdb, err := gorm.Open(
 		postgres.Open(dsn),
 		&gorm.Config{
 			NamingStrategy: schema.NamingStrategy{
@@ -32,25 +38,30 @@ func CreateDb(dsn string, schemaName string) (db *gorm.DB) {
 	if err != nil {
 		panic(err)
 	}
-	return db
+	return &DbService{*gdb}
 }
 
-func CreateDao[T domain.Data | domain.User](db *gorm.DB) Api[T] {
-	return &DaoService[T]{db: db}
+func CreateDao[T domain.Data | domain.User](db DbApi) DaoApi[T] {
+	db.(DbService)
+	return &DaoService[T]{db: db.(DbService)}
 }
 
-func (ds *DaoService[T]) Insert(val *T) (*T,error) {
-	return nil, nil;
+func (db *DbService) Query(func(args... interface{}) *gorm.DB) *gorm.DB {
+	
 }
 
-func (ds *DaoService[T]) GetByField (field string, val interface{}) (*T,error) {
-	return nil, nil;
+func (ds *DaoService[T]) Insert(val *T) (*T, error) {
+	return nil, nil
 }
 
-func (ds *DaoService[T]) DeleteByField (field string, val interface{}) (*T,error) {
-	return nil, nil;
+func (ds *DaoService[T]) GetByField(field string, val interface{}) (*T, error) {
+	return nil, nil
 }
 
-func (ds *DaoService[T]) UpdateField(id int, field string, val interface{}) (*T, error){
-	return nil, nil;
+func (ds *DaoService[T]) DeleteByField(field string, val interface{}) (*T, error) {
+	return nil, nil
+}
+
+func (ds *DaoService[T]) UpdateField(id int, field string, val interface{}) (*T, error) {
+	return nil, nil
 }
